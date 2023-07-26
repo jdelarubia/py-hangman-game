8dc45b5265793cf26e9a229131b74bd8f363623c
jdelarubia <jdelarubia@gmail.com>
2023-07-26 21:31:49 +0200

    test add user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,9 +9,23 @@ class TestUser(unittest.TestCase):
     def test_nickname_dont_match(self) -> None:
         current = self.test_user.nickname
         expected = "anything"
-        self.assertRaises(ValueError)
+        with self.assertRaises(AssertionError):
+            assert current == expected
 
     def test_nickname_match(self) -> None:
         current = self.test_user.nickname
         expected = "johnny"
-        self.assertEqual(current, expected, "nicknames don't match")
+        self.assertEqual(
+            current, expected, f"nicknames don't match, {current} != {expected}"
+        )
+
+    def test_initial_score_is_zero(self) -> None:
+        current = self.test_user.score
+        expected = 0
+        self.assertEqual(current, expected, f"initial score must be {expected}")
+
+    def test_add_points_to_score(self) -> None:
+        self.test_user.add_score(1)
+        current = self.test_user.score
+        expected = 1
+        self.assertEqual(current, expected, f"score must be {expected}")
