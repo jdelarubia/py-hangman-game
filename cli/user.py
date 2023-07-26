1f917665a9f8d854030015dfe26b699e44d0c299
jdelarubia <jdelarubia@gmail.com>
2023-07-26 21:23:04 +0200

    create user class

---
+++ cli/user.py
@@ -0,0 +1,23 @@
+"""user.py
+"""
+
+
+class User:
+    def __init__(self, nickname: str, score: int) -> None:
+        self._nickname = nickname.lower()
+        self._score = score
+
+    @property
+    def nickname(self) -> str:
+        return self._nickname
+
+    @property
+    def score(self) -> int:
+        return self._score
+
+    @score.setter
+    def score(self, new_score: int) -> int:
+        self._score = new_score
+
+    def add_score(self, points: int):
+        self._score += points
