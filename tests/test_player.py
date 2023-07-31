4589f1a2319451773c75e1dc8559caf914324c79
jdelarubia <jdelarubia@gmail.com>
2023-07-31 22:17:29 +0200

    fix player tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -4,7 +4,7 @@ from cli.player import Player
 
 class TestUser(unittest.TestCase):
     def setUp(self) -> None:
-        self.test_user = Player(nickname="Johnny", score=0)
+        self.test_user = Player(nickname="Johnny", wins=0, losses=0)
 
     def test_nickname_dont_match(self) -> None:
         current = self.test_user.nickname
@@ -18,12 +18,12 @@ class TestUser(unittest.TestCase):
         self.assertEqual(current, expected, f"nicknames don't match, {current} != {expected}")
 
     def test_initial_score_is_zero(self) -> None:
-        current = self.test_user.score
+        current = self.test_user.wins
         expected = 0
         self.assertEqual(current, expected, f"initial score must be {expected}")
 
     def test_add_points_to_score(self) -> None:
-        self.test_user.add_score(1)
-        current = self.test_user.score
+        self.test_user.add_win()
+        current = self.test_user.wins
         expected = 1
         self.assertEqual(current, expected, f"score must be {expected}")
